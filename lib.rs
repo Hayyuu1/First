89f8638d4f062ae5f55ca4d44755b7a66e53e09d
Hayyuu1 <56871603+Hayyuu1@users.noreply.github.com>
2020-01-19 09:33:21 -0500

    Assignment

---
+++ lib.rs
@@ -0,0 +1,11 @@
+ pub mod student{
+    pub mod information{
+        pub fn data(){
+            println!("Student Name is abc");
+            println!("Student Roll number is 123");
+        }
+    }
+  
+  
+  }
+  
